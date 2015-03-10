694ad935a59d1f908bd993b9ce8317434fdf6718
Marcus Nitzschke <nitzschke@informatik.uni-leipzig.de>
2015-03-10 15:48:35 +0100

    modified settings pages; fixed translation

--- js/config.js
+++ js/config.js
@@ -12,14 +12,14 @@ angular
 	    .when('/about', { templateUrl: 'views/about.html', title: "Über" })
 	    .when('/drug/add', { templateUrl: 'views/drug_form.html', controller: 'DrugFormCtrl', title: "Medikament hinzufügen", returnMenu: true})
 	    .when('/drug/edit/:id', { templateUrl: 'views/drug_form.html', controller: 'DrugFormCtrl', title: "Medikament bearbeiten" })
-	    .when('/debug', { templateUrl: 'views/debug.html', controller: 'DebugCtrl' })
+	    .when('/debug', { templateUrl: 'views/debug.html', controller: 'DebugCtrl', title: "Debug" })
 	    .when('/settings', { templateUrl: 'views/settings.html', title: "Einstellungen" })
 	    .otherwise({ redirectTo: '/' });
     })
     .config(function ($translateProvider) {
 	$translateProvider
 	    .useStaticFilesLoader({
-		prefix: 'lang/',
+		prefix: '/lang/',
 		suffix: '.json'
 	    })
 	    .determinePreferredLanguage();
